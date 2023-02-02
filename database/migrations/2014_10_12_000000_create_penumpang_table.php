46b2437f63e470384b0a45ccddc253e5e604cc53
lintangfauzi <fauzilintang2002@gmail.com>
2023-02-02 18:25:49 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_penumpang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penumpang', function (Blueprint $table) {
+            $table->integerIncrements('id_petugas');
+            $table->string('name_penumpamg');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('alamat_penumpang');
+            $table->integer('id_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('petugas');
+    }
+};
